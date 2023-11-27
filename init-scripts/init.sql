3b93baa61a4efc7bb18ebc24bab844559913babf
Study19999 <maria.study.now@tutanota.com>
2023-11-27 14:39:25 +0200

    Updated the init.sql file.

--- init-scripts/init.sql
+++ init-scripts/init.sql
@@ -1,12 +1,18 @@
+DO $$ 
+BEGIN
+   RAISE NOTICE 'Executing CREATE TABLE statement...';
+   CREATE TABLE IF NOT EXISTS product (
+       id integer NOT null GENERATED ALWAYS AS IDENTITY (INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1),
+       title character varying,
+       price bigint,
+       stock integer
+   );
+   RAISE NOTICE 'CREATE TABLE statement executed successfully.';
 
-CREATE TABLE IF NOT EXISTS product (
-    id INTEGER PRIMARY KEY,
-    title CHARACTER VARYING,
-    price BIGINT,
-    stock INTEGER
-);
-
-INSERT INTO product (title, price, stock) VALUES
-    ('Product 1', 19.99, 100),
-    ('Product 2', 29.99, 50),
-    ('Product 3', 9.99, 200);
+   RAISE NOTICE 'Inserting data into the product table...';
+   INSERT INTO product (title, price, stock) VALUES
+       ('Product 1', 19.99, 100),
+       ('Product 2', 29.99, 50),
+       ('Product 3', 9.99, 200);
+   RAISE NOTICE 'Data inserted successfully.';
+END $$;
